6897e584f16ea32f64cec745c69540d58581fdb0
manolis <eemmanoua@csd.auth.gr>
2021-01-15 16:43:44 +0200

    Added authors and version on the Javadoc of the Main class.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 
 /**
  * From here starts all the Buzz Game
+ * @author Nikolaos Tzavidas 3442
+ * @author Emmanouil Eleftheriadis 3415
+ * @version 2.0 15/1/2020
  */
 public class Main {
     public static void main(String[] args) throws IOException {
